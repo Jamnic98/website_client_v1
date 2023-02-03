afc66d604507dfbd5f82d8be469ed862af83c833
Jamnic98 <jamiestimpson30@gmail.com>
2023-02-03 00:20:57 +0000

    refactor

--- components/Navbar/Navbar.tsx
+++ components/Navbar/Navbar.tsx
@@ -1,5 +1,5 @@
-import React, { useState } from 'react'
-import { MobileMenuToggle, Navlink, NavLogo } from '..'
+import { type FC, useState } from 'react'
+// import { MobileMenuToggle, Navlink, NavLogo } from '..'
 import { type NavlinkData } from '../../types/global'
 import classNames from 'classnames'
 import styles from './Navbar.module.css'
@@ -8,26 +8,26 @@ export interface NavbarProps {
   navlinkObjArr: NavlinkData[]
 }
 
-export const Navbar = ({ navlinkObjArr }: NavbarProps) => {
+export const Navbar: FC<NavbarProps> = ({ navlinkObjArr }) => {
   const [isOpen, setIsOpen] = useState(false)
   const handleClick = () => setIsOpen(!isOpen)
 
   return (
     <nav className={styles.navbar} data-testid='navbar' aria-label='navbar'>
       <span className='container'>
-        <MobileMenuToggle handleClick={handleClick} isOpen={isOpen} />
-        <NavLogo />
+        {/* <MobileMenuToggle handleClick={handleClick} isOpen={isOpen} /> */}
+        {/* <NavLogo /> */}
         <span
           className={classNames(styles.navlinks, { [styles.open]: isOpen })}
           aria-label='navlinks'
         >
-          {navlinkObjArr.map((navlinkObj, index) => (
+          {/* {navlinkObjArr.map((navlinkObj, index) => (
             <Navlink
               onClick={() => setIsOpen(false)}
               navlinkObj={navlinkObj}
               key={index}
             />
-          ))}
+          ))} */}
         </span>
       </span>
     </nav>
