b86d98b29ab748169ec0fc03eac8e8615e3c2ee4
Jamnic98 <jamiestimpson30@gmail.com>
2024-05-19 19:05:11 +0100

    update bio

--- pages/about.tsx
+++ pages/about.tsx
@@ -30,8 +30,8 @@ const About = () => (
 				<hr className={styles.hr} />
 				<section className={styles.description}>
 					<p>
-						Hello, I'm Jamie, a (mostly) self-taught Software Engineer with Full
-						Stack and DevOps experience based in London. Connect with me on{' '}
+						I work as a Software Engineer based in London and have Full Stack
+						and DevOps. Connect with me on{' '}
 						<a
 							href={personalLinkedInURL}
 							target="_blank"
@@ -40,7 +40,7 @@ const About = () => (
 						>
 							LinkedIn
 						</a>
-						, or check out my repositories on{' '}
+						and check out my repositories on{' '}
 						<a
 							href={personalGithubReposURL}
 							target="_blank"
@@ -52,23 +52,23 @@ const About = () => (
 						.
 					</p>
 					<p>
-						My experience covers various programming languages and frameworks,
-						including TypeScript, React, Python, and AWS technologies, in
-						addtion to web development tools like React, Flask and Node.js. I
-						also have experience in Data Science and A.I tools such as NumPy,
-						pandas and Scikit-Learn, as well as DevOps tools like Docker,
-						Ansible, and GitLab CI/CD pipelines. Please take a look at some of
-						the{' '}
+						My experience covers several programming languages but the ones I am
+						most comfortable with are TypeScript and Python, as I have used them
+						the most in building full-stack applications with React, Node.js and
+						Flask. I also have experience with data science and machine learning
+						technologies like NumPy, pandas and Scikit-Learn, as well as DevOps
+						tools like Docker, Ansible, and Git CI/CD pipelines. Please take a
+						look at some of the{' '}
 						<Link className={styles.projectLink} href="/projects">
 							personal projects
 						</Link>{' '}
 						I have worked on.
 					</p>
 					<p>
-						Since 2020, I have been running regularly, aspiring to one day
-						complete an ultramarathon (&#8805;50km). On the 22nd of October
-						2023, I ran the Battersea Park Marathon and raised £275 for The
-						Alzheimer's Society! To explore my running stats, click{' '}
+						During lockdown in 2020, developed a regular running routine,
+						aspiring to one day complete an ultramarathon (&#8805;50km). On the
+						22nd of October 2023, I ran the Battersea Park Marathon and raised
+						£275 for The Alzheimer's Society! To explore my running stats, click{' '}
 						<Link className={styles.projectLink} href="/logs/running">
 							here
 						</Link>
